c0d725e1b483ec45c326e7744cb7aed6dbeeb182
AlexandrTarasyuk <alextarasyuk79@gmail.com>
2017-06-29 08:34:33 +0300

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/alex/MediaPlayerColibri/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.alex.MediaPlayerColibri;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.devcolibri.MediaPlayerColibri";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
